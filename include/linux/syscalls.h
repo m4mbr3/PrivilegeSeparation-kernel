ab9d4226e97d1bca3262989b5c3a5a3280563718
Andrea Mambretti <m4mbr3@gmail.com>
2015-05-01 07:21:19 -0400

    Updated tracemalloc syscall to mmap and unmap
    It works as mmap and unmap and adds and removes chunks info into the program
    task_struct all in once

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -901,5 +901,6 @@ asmlinkage long sys_kcmp(pid_t pid1, pid_t pid2, int type,
 asmlinkage long sys_finit_module(int fd, const char __user *uargs, int flags);
 asmlinkage long sys_ps_info(struct PrivSec_t*, int level);
 asmlinkage long sys_ps_switch(int new_level);
-asmlinkage long sys_ps_tracemalloc (void *ptr, int size, char *cmd);
+asmlinkage unsigned long sys_ps_tracemalloc (unsigned long addr, unsigned long len,
+            int prot, int flags, char *cmd, int privlev);
 #endif
--- kernel/ps_tracemalloc.c
+++ kernel/ps_tracemalloc.c
@@ -1,5 +1,5 @@
 /*
- * ps_tracemalloc - Syscall that trace for each malloc and free 
+ * ps_tracemalloc - Syscall that trace for each malloc and free
  * the dynamic data allocation and protect them from improper access
  *
  */
@@ -11,80 +11,102 @@
 #include <linux/string.h>
 
 
-asmlinkage long sys_ps_tracemalloc (void *ptr, int size, char *cmd) {
+asmlinkage unsigned long sys_ps_tracemalloc (unsigned long addr,
+                                    unsigned long len,
+                                    int prot,
+                                    int flags,
+                                    char *cmd,
+                                    int privlev) {
+
     int found = 0;
     struct PrivSec_dyn_t *curr = NULL;
     struct PrivSec_dyn_t *new = NULL;
     struct PrivSec_dyn_t *next = NULL;
+    unsigned long ptr;
+
     if (current->ps_info_h == NULL) {
         printk (KERN_ERR "PS_TRACEMALLOC: The program %u is not designed to use the "
                 "Privilege Separation system\n", current->pid);
-        return 0;
+        return -EINVAL;
     }
-    
-    
-    if (strncmp(cmd, "MALLOC", 6) == 0) {
-       printk (KERN_INFO "PS_TRACEMALLOC: The command provided is MALLOC \n"); 
-       printk (KERN_INFO "PS_TRACEMALLOC: %x address, %u size\n", (unsigned int) ptr, size); 
+
+    if (strncmp(cmd,"MMAP", 4) == 0) {
+
+       ptr = sys_mmap_pgoff(addr, len, prot, flags, -1, (off_t)0);
+
+       printk (KERN_INFO "PS_TRACEMALLOC: The command provided is MMAP \n");
+       printk (KERN_INFO "PS_TRACEMALLOC: %lx ptr, %lu size\n", ptr, len);
+
+       if (!IS_ERR((void *) ptr))
+           force_successful_syscall_return();
+
        if (current->ps_dyn_info_h == NULL) {
             //create a new element that will be the first of the list
-            struct PrivSec_dyn_t *new = 
+            struct PrivSec_dyn_t *new =
                 (struct PrivSec_dyn_t *) kmalloc(sizeof(struct PrivSec_dyn_t),
                                           GFP_ATOMIC);
-            new->ps_level = current->ps_level;
-            new->size = size;
-            new->mem = ptr;
+            new->ps_level = privlev;
+            new->size = len;
+            new->mem = (void *) ptr;
             new->next = NULL;
             current->ps_dyn_info_h = new;
-            return 1;
+            return ptr;
        }
        curr = current->ps_dyn_info_h;
        while (curr->next != NULL) curr = curr->next;
-       new = (struct PrivSec_dyn_t *) kmalloc(sizeof(struct PrivSec_dyn_t), 
+       new = (struct PrivSec_dyn_t *) kmalloc(sizeof(struct PrivSec_dyn_t),
                                       GFP_ATOMIC);
-       new->ps_level = current->ps_level;
-       new->size = size;
-       new->mem = ptr;
+       new->ps_level = privlev;
+       new->size = len;
+       new->mem = (void *) ptr;
        new->next = NULL;
        curr->next = new;
+       return ptr;
     }
-    else if (strncmp(cmd, "FREE", 4) == 0) {
-        printk (KERN_INFO "PS_TRACEMALLOC: The command provided is FREE \n");
-        printk (KERN_INFO "PS_TRACEMALLOC: %x address, %u size\n", (unsigned int)ptr, size);
+    else if (strncmp(cmd, "MUNMAP", 6) == 0) {
+
+        printk (KERN_INFO "PS_TRACEMALLOC: The command provided is MUNMAP \n");
+        printk (KERN_INFO "PS_TRACEMALLOC: %lx address, %lu size\n", addr, len);
+
         if (current->ps_dyn_info_h == NULL) return -1; //case zero elements.
+
         curr = current->ps_dyn_info_h;
+
         if (curr->next == NULL) {
             //case only one element inside the list
-            if (curr->mem == ptr) {
+            if (curr->mem == (void *)addr) {
                 kfree(curr);
                 current->ps_dyn_info_h = NULL;
+                sys_munmap (addr, len);
                 return 1;
             }
             else {
-                return -1;
+                return -EINVAL;
             }
         }
         next = curr->next;
         if (next->next == NULL) {
-           //case two elements inside the list. 
-           if (curr->mem == ptr) {
+           //case two elements inside the list.
+           if (curr->mem == (void *) addr) {
                 current->ps_dyn_info_h = next;
                 kfree(curr);
+                sys_munmap (addr, len);
                 return 1;
            }
-           else if (next->mem == ptr) {
+           else if (next->mem == (void *) addr) {
                 curr->next = NULL;
                 kfree(next);
+                sys_munmap (addr, len);
                 return 1;
            }
            else {
-               return -1;
+               return -EINVAL;
            }
         }
         found = 0;
         while(!found && next != NULL) {
-            if (curr->mem == ptr) found=2;
-            else if (next->mem == ptr ) found=1;
+            if (curr->mem == (void *) addr) found=2;
+            else if (next->mem == (void *) addr ) found=1;
             else {
                 next = next->next;
                 curr = curr->next;
@@ -93,18 +115,20 @@ asmlinkage long sys_ps_tracemalloc (void *ptr, int size, char *cmd) {
         if (found == 2) {
             current->ps_dyn_info_h = next;
             kfree(curr);
+            sys_munmap (addr, len);
             return 1;
         }
         if (found == 1) {
             curr->next = next->next;
-            kfree(next); 
+            kfree(next);
+            sys_munmap (addr, len);
             return 1;
         }
-        return -1;
+        return -EINVAL;
     }
     else {
         printk (KERN_ERR "PS_TRACEMALLOC: Unknown command provided\n");
-        return -1;
+        return -EINVAL;
     }
-    return -1;
+    return -EINVAL;
 }
